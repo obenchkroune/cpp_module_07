37b87ab1f8e5019bfe108c3829c9a38a585b67d8
OUSSAMA BENCHKRON <obenchkroune01@gmail.com>
2024-09-26 10:46:27 +0100

    add ex00

---
+++ ex00/whatever.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+template<class T>
+void swap(T& a, T& b) {
+	T temp = a;
+	a = b;
+	b = temp;
+}
+
+template<class T>
+T min(const T& a, const T& b) {
+	return (a < b ? a : b);
+}
+
+template<class T>
+T max(const T& a, const T& b) {
+	return (a > b ? a : b);
+}
